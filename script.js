05ea5f2721bb2bd0d28f9fa75f093c83633e14b2
Leslie Ann Lim <Lalim@Leslies-MacBook-Air.local>
2016-02-27 13:40:22 -0800

    created quizGenerator Constructor #9 #10

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 /**
  * Created by Lalim on 2/27/16.
  */
+var questionArray = [];
+function quizGenerator (){
+
+    this.makequestions= function(questions,weight,path){
+        this.questions = questions;
+        this.weight = weight;
+        this.path = path;
+        this.answer = null;
+        pushQuestion(this);
+    };
+
+    function pushQuestion (question){
+        questionArray.push(question);
+    }
+
+}
+var quiz = new quizGenerator();
+quiz.makequestions('What was your experience with JavaScript?',45,'back');
+quiz.makequestions('Do you like analogies and metaphors?',50,'back');
+quiz.makequestions('Do you like working with groups?',50,'front');
+quiz.makequestions('Do you enjoy CSS and Bootstrap?',45,'front');
+console.log (questionArray);
