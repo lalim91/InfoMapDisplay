1c487c586e5cd4a71a9f77e156928cb428ee1eca
stan wielga <svwielga4@gmail.com>
2016-02-27 11:01:57 -0800

    function map?

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
 /**
  * Created by Lalim on 2/27/16.
  */
+
+function splashScreenToUltimateDecision(){
+
+};
+function appendDan(){
+
+};
+function appendEric(){
+
+};
+function frontEndPicked(){
+
+};
+function backEndPicked(){
+
+};
+function headClicked(){
+
+};
+function eyesClicked(){
+
+};
+function earsClicked(){
+
+};
+function noseClicked(){
+
+};
+function mouthClicked(){
+
+};
+function armsClicked(){
+
+};
+function handsClicked(){
+
+};
+function torsoClicked(){
+
+};
+function legsClicked(){
+
+};
+function footClicked(){
+
+};
