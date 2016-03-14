3784d80060fac0eadfe0d94ec02cbacedae4a921
Neal Cloud <neal.cloud@gmail.com>
2016-03-13 18:10:39 -0700

    Angular Update -NC

--- main.js
+++ main.js
@@ -1,111 +1,116 @@
 "use strict";
 
-var model = new Model();
-var view = new View();
-var controller = new Controller();
+    angular.module("quizApp", ['ngRoute'])
+        .config(function($routeProvider){
+            $routeProvider
+                .when('/home', {
+                    templateUrl: 'splash.html'
+                })
+                .when('/quiz', {
+                    templateUrl: 'quiz.html',
+                    controller: 'quizController',
+                    controllerAs: "qc"
+                })
+                .when('/results', {
+                    templateUrl: 'results.html'
+                })
+                .otherwise({
+                    redirectTo: '/home'
+                })
+        })
 
-function Model() {
-    this.listOfQuestions = [{
-        question: "Do you like to see and interact with your web page?",
-        weight: 80,
-        path: "front",
-        answer: null},
-        {
-        question: "Do enjoy styling web pages?",
-        weight: 50,
-        path: "front",
-        answer: null},
-        {
-        question: "Do you care about the user's experience with web page?",
-        weight: 50,
-        path: "front",
-        answer: null},
-        {
-        question: "Do you hate HTML?",
-        weight: 50,
-        path: "back",
-        answer: null},
-        {
-        question: "Do you hate bootstrap",
-        weight: 50,
-        path: "back",
-        answer: null},
-        {
-        question: "Do you think DOM creation sucks ass?",
-        weight: 50,
-        path: "back",
-        answer: null}
-    ];
-    this.currentQuestion = null;
-    this.frontEnd = 0;
-    this.backEnd = 0;
-    this.index = 0;
+        .service("qServ", function(){
 
-}
+            this.listOfQuestions = [{
+                question: "Do you like to see and interact with your web page?",
+                weight: 80,
+                path: "front",
+                answer: null},
+                {
+                    question: "Do enjoy styling web pages?",
+                    weight: 50,
+                    path: "front",
+                    answer: null},
+                {
+                    question: "Do you care about the user's experience with web page?",
+                    weight: 50,
+                    path: "front",
+                    answer: null},
+                {
+                    question: "Do you hate HTML?",
+                    weight: 50,
+                    path: "back",
+                    answer: null},
+                {
+                    question: "Do you hate bootstrap",
+                    weight: 50,
+                    path: "back",
+                    answer: null},
+                {
+                    question: "Do you think DOM creation sucks ass?",
+                    weight: 50,
+                    path: "back",
+                    answer: null}
+            ];
+            this.currentQuestion = null;
+            this.frontEnd = 0;
+            this.backEnd = 0;
+            this.index = 0;
+        })
 
-function View() {
-    var self = this;
+        .controller('quizController', function (qServ) {
+            //this.test = qServ.listOfQuestions[2].question;
+            var self = this;
+            this.currentQ = qServ.currentQuestion;
+            this.showNewQuestion = function(){
+                console.log("new question");
+                if (qServ.index < qServ.listOfQuestions.length) {
+                    qServ.currentQuestion = qServ.listOfQuestions[qServ.index];
+                    this.currentQ = qServ.currentQuestion.question;
+                }
+                else {
+                    qServ.currentQuestion = null;
+                    if (qServ.backEnd > qServ.frontEnd){
+                        this.currentQ ="You should pick this backEnd!";
+                    }
+                    else if (qServ.backEnd < qServ.frontEnd){
+                        this.currentQ = "You should pick this frontEnd!";
+                    }
+                    else{
+                        this.currentQ = "Just Walk!";
+                    }
+                }
+            };
 
-    this.showNewQuestion = function(){
-        if (model.index < model.listOfQuestions.length) {
-            $("#quizQuestion").empty();
-            this.appendQuestion(model.listOfQuestions[model.index].question);
-            model.currentQuestion = model.listOfQuestions[model.index];
-        }
-        else {
-            model.currentQuestion = null;
-            $("#quizQuestion").empty();
-            if (model.backEnd > model.frontEnd){
-                self.appendQuestion("You should pick this backEnd!");
+
+            this.addToPath = function (question) {
+                if (question.path === "front") {
+                    if (question.answer === "yes") {
+                        qServ.frontEnd += question.weight;
+                    }
+                    else if (question.answer === "no") {
+                        qServ.backEnd += question.weight;
+                    }
+                }
+                else if (question.path === "back") {
+                    if (question.answer === "yes") {
+                        qServ.backEnd += question.weight;
+                    }
+                    else if (question.answer === "no") {
+                        qServ.frontEnd += question.weight;
+                    }
+                }
             }
-            else if (model.backEnd < model.frontEnd){
-                self.appendQuestion("You should pick this frontEnd!");
+            this.nextQuestion = function(){
+                console.log("curret: ", self.currentQ , qServ.currentQuestion);
+                if(qServ.currentQuestion) {
+                    qServ.currentQuestion.answer = $('input[name=optradio]:checked').val();
+                    self.addToPath(qServ.listOfQuestions[qServ.index]);
+                    qServ.index++;
+                    self.showNewQuestion();
+                }
+                else{
+                    self.showNewQuestion();
+                }
             }
-            else{
-                self.appendQuestion("Just Walk!");
-            }
-        }
-    };
-
-    this.appendQuestion = function(questionToShow){
-        var div = $("<div>",{
-            text: questionToShow
         });
-
-        $("#quizQuestion").append(div);
-    };
-}
-
-function Controller() {
-    this.addToPath = function (question) {
-        if (question.path === "front") {
-            if (question.answer === "yes") {
-                model.frontEnd += question.weight;
-            }
-            else if (question.answer === "no") {
-                model.backEnd += question.weight;
-            }
-        }
-        else if (question.path === "back") {
-            if (question.answer === "yes") {
-                model.backEnd += question.weight;
-            }
-            else if (question.answer === "no") {
-                model.frontEnd += question.weight;
-            }
-        }
-    }
-}
-
-$(document).ready(function() {
-    view.showNewQuestion();
-
-    $('#next').click(function() {
-        if(model.currentQuestion) {
-            model.currentQuestion.answer = $('input[name=optradio]:checked').val();
-            controller.addToPath(model.listOfQuestions[model.index]);
-            model.index++;
-            view.showNewQuestion();
-        }
-    });
-});
