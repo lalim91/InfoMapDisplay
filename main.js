850aa120fa29ea92c0a10fa6d8f23ebcbb511ce3
Timothy Nguyen <shimoshixp@gmail.com>
2016-03-28 16:31:16 -0700

    working on add to path function

--- main.js
+++ main.js
@@ -1,151 +1,165 @@
-
 "use strict";
 
-    angular.module("quizApp", ['ngRoute'])
-        .config(function($routeProvider){
-            $routeProvider
-                .when('/home', {
-                    templateUrl: 'splash.html'
-                })
-                .when('/quiz', {
-                    templateUrl: 'quiz.html',
-                    controller: 'quizController',
-                    controllerAs: "qc"
-                })
-                .when('/results', {
-                    templateUrl: 'results.html'
-                })
-                .otherwise({
-                    redirectTo: '/home'
-                })
-        })
-        .directive('myResult', function(){
-             return{
-                 restrict: 'E',
-                 templateUrl: "result.html",
-                 scope:true,
-                 controller: function(resultsService){
-                   this.outcome={
-                       side: resultsService.results,
-                       description: resultsService.description,
-                       image: resultsService.image
-                   }
+angular.module("quizApp", ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/home', {
+                templateUrl: 'splash.html'
+            })
+            .when('/quiz', {
+                templateUrl: 'quiz.html',
+                controller: 'quizController',
+                controllerAs: "qc"
+            })
+            .when('/results', {
+                templateUrl: 'results.html'
+            })
+            .otherwise({
+                redirectTo: '/home'
+            })
+    })
+    .directive('myResult', function () {
+        return {
+            restrict: 'E',
+            templateUrl: "result.html",
+            scope: true,
+            controller: function (resultsService) {
+                this.outcome = {
+                    side: resultsService.results,
+                    description: resultsService.description,
+                    image: resultsService.image
+                }
 
-                 },
-                 controllerAs:'rc'
-             };
-         })
-        .service("qServ", function(){
+            },
+            controllerAs: 'rc'
+        };
+    })
+    .service("qServ", function () {
 
-            this.listOfQuestions = [{
-                question: "Do you like to see and interact with your web page?",
-                weight: 80,
+        this.listOfQuestions = [{
+            question: "Do you like to see and interact with your web page?",
+            weight: 80,
+            path: "front",
+            answer: null
+        },
+            {
+                question: "Do enjoy styling web pages?",
+                weight: 50,
                 path: "front",
-                answer: null},
-                {
-                    question: "Do enjoy styling web pages?",
-                    weight: 50,
-                    path: "front",
-                    answer: null},
-                {
-                    question: "Do you care about the user's experience with web page?",
-                    weight: 50,
-                    path: "front",
-                    answer: null},
-                {
-                    question: "Do you hate HTML?",
-                    weight: 50,
-                    path: "back",
-                    answer: null},
-                {
-                    question: "Do you hate bootstrap",
-                    weight: 50,
-                    path: "back",
-                    answer: null},
-                {
-                    question: "Do you think DOM creation sucks ass?",
-                    weight: 50,
-                    path: "back",
-                    answer: null}
-            ];
-            this.currentQuestion = null;
-            this.frontEnd = 0;
-            this.backEnd = 0;
-            this.index = 0;
-        })
-        .service("resultsService", function(){
-            this.results = "";
-            this.description = "";
-            this.image = "";
+                answer: null
+            },
+            {
+                question: "Do you care about the user's experience with web page?",
+                weight: 50,
+                path: "front",
+                answer: null
+            },
+            {
+                question: "Do you hate HTML?",
+                weight: 50,
+                path: "back",
+                answer: null
+            },
+            {
+                question: "Do you hate bootstrap",
+                weight: 50,
+                path: "back",
+                answer: null
+            },
+            {
+                question: "Do you think DOM creation sucks ass?",
+                weight: 50,
+                path: "back",
+                answer: null
+            }
+        ];
+        this.currentQuestion = null;
+        this.frontEnd = 0;
+        this.backEnd = 0;
+        this.index = 0;
+    })
+    .service("resultsService", function () {
+        this.results = "";
+        this.description = "";
+        this.image = "";
 
-        })
+    })
 
 
-        .controller('quizController', function ($location, qServ, resultsService) {
-            //this.test = qServ.listOfQuestions[2].question;
-            var self = this;
-            this.currentQ = qServ.currentQuestion;
-            this.showNewQuestion = function(){
-                console.log("new question");
+    .controller('quizController', function ($location, qServ, resultsService) {
+        //this.test = qServ.listOfQuestions[2].question;
+        var self = this;
+        this.currentQ = qServ.currentQuestion;
+        this.showNewQuestion = function () {
+            console.log("new question");
 
-                if (qServ.index < qServ.listOfQuestions.length) {
-                    qServ.currentQuestion = qServ.listOfQuestions[qServ.index];
-                    this.currentQ = qServ.currentQuestion.question;
+            if (qServ.index < qServ.listOfQuestions.length) {
+                qServ.currentQuestion = qServ.listOfQuestions[qServ.index];
+                this.currentQ = qServ.currentQuestion.question;
+            }
+            else {
+                qServ.currentQuestion = null;
+                if (qServ.backEnd > qServ.frontEnd) {
+                    resultsService.description = "You should pick this backEnd!";
+                    resultsService.results = "back";
+                    resultsService.image = "images/Dan.png";
+                    window.location.href = '#results';
+                }
+                else if (qServ.backEnd < qServ.frontEnd) {
+                    resultsService.description = "You should pick this frontEnd!";
+                    resultsService.results = "front";
+                    resultsService.image = "images/Eric.png";
+                    window.location.href = '#results';
                 }
                 else {
-                    qServ.currentQuestion = null;
-                    if (qServ.backEnd > qServ.frontEnd){
-                        resultsService.description ="You should pick this backEnd!";
-                        resultsService.results = "back";
-                        resultsService.image = "images/Dan.png";
-                        window.location.href= '#results';
-                    }
-                    else if (qServ.backEnd < qServ.frontEnd){
-                        resultsService.description = "You should pick this frontEnd!";
-                        resultsService.results = "front";
-                        resultsService.image = "images/Eric.png";
-                        window.location.href= '#results';
-                    }
-                    else{
-                        resultsService.description = "Just Walk!";
-                        resultsService.results = "none";
-                        window.location.href= '#results';
-                    }
-                }
-            };
-
-            this.addToPath = function (question) {
-                if (question.path === "front") {
-                    if (question.answer === "yes") {
-                        qServ.frontEnd += question.weight;
-                    }
-                    else if (question.answer === "no") {
-                        qServ.backEnd += question.weight;
-                    }
-                }
-                else if (question.path === "back") {
-                    if (question.answer === "yes") {
-                        qServ.backEnd += question.weight;
-                    }
-                    else if (question.answer === "no") {
-                        qServ.frontEnd += question.weight;
-                    }
-                }
-            };
-            this.nextQuestion = function(){
-
-                console.log("current: ", self.currentQ , qServ.currentQuestion);
-
-
-                if(qServ.currentQuestion) {
-                    qServ.currentQuestion.answer = $('input[name=optradio]:checked').val();
-                    self.addToPath(qServ.listOfQuestions[qServ.index]);
-                    qServ.index++;
-                    $('input[name=optradio]').attr('checked',false);
-                    self.showNewQuestion();
-                }
-                else{
-                    self.showNewQuestion();
+                    resultsService.description = "Just Walk!";
+                    resultsService.results = "none";
+                    window.location.href = '#results';
                 }
             }
-        });
+        };
+
+        this.addToPath = function (question) {
+            if (question.path === "front") {
+                if (question.answer === "Strongly Agree") {
+                    qServ.frontEnd += question.weight;
+                }
+                else if (question.answer === "Agree") {
+                    qServ.backEnd += question.weight;
+                }
+                else if (question.answer === "Neutral") {
+                    qServ.backEnd += question.weight;
+                }
+                else if (question.answer === "Disagree") {
+                    qServ.backEnd += question.weight;
+                }
+                else if (question.answer === "Strongly Disagree") {
+                    qServ.backEnd += question.weight;
+                }
+            }
+            else if (question.path === "back") {
+                if (question.answer === "yes") {
+                    qServ.backEnd += question.weight;
+                }
+                else if (question.answer === "no") {
+                    qServ.frontEnd += question.weight;
+                }
+            }
+        };
+        this.nextQuestion = function () {
+
+            console.log("current: ", self.currentQ, qServ.currentQuestion);
+
+
+            if (qServ.currentQuestion) {
+                qServ.currentQuestion.answer = $('input[name=optradio]:checked').val();
+                self.addToPath(qServ.listOfQuestions[qServ.index]);
+                qServ.index++;
+                $('input[name=optradio]').attr('checked', false);
+                self.showNewQuestion();
+            }
+            else {
+                self.showNewQuestion();
+            }
+        }
+    });
